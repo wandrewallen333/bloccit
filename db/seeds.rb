84379d4c7611011b5d580a3766fa764698e57cb7
Andrew Allen <w.andrewallen@gmail.com>
2017-12-08 19:34:12 -0600

    Assignment 16 Rails Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,10 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "This is Andrew's Title", body: "This is Andrew's body")
+puts "#{Post.count}" 
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
