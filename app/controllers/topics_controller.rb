08121b447cb4469d062ea77f0dc1f34d06954c8f
Andrew Allen <w.andrewallen@gmail.com>
2018-02-15 17:37:02 -0600

    Changed to ProduceIt

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show,]
 
   def index
     @topics = Topic.all
@@ -61,10 +61,10 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-     unless current_user.admin?
+     unless current_user.admin? || current_user
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
      end
-   end   
+   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,67 +11,67 @@ end
 users = User.all
 
 #Create Topics
-15.times do
-  Topic.create!(
-    name: RandomData.random_sentence,
-    description: RandomData.random_paragraph
-  )
-end
-topics = Topic.all
+#15.times do
+#  Topic.create!(
+#    name: RandomData.random_sentence,
+#    description: RandomData.random_paragraph
+#  )
+#end
+#topics = Topic.all
 
 # Create Posts
-50.times do
-  post = Post.create!(
-    user: users.sample,
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
+#50.times do
+#  post = Post.create!(
+#    user: users.sample,
+#    topic: topics.sample,
+#    title: RandomData.random_sentence,
+#    body: RandomData.random_paragraph
+#  )
 
-  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
-  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
-end
-posts = Post.all
+#  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+#  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+#end
+#posts = Post.all
 
 # Create Comments
-100.times do
-  Comment.create!(
-    user: users.sample,
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
+#100.times do
+#  Comment.create!(
+#    user: users.sample,
+#    post: posts.sample,
+#    body: RandomData.random_paragraph
+#  )
+#end
 
 # Create Questions
-100.times do
-  Question.create!(
-    title: RandomData.random_sentence,
-    body:  RandomData.random_paragraph,
-    resolved: false
-  )
-end
+#100.times do
+#  Question.create!(
+#    title: RandomData.random_sentence,
+#    body:  RandomData.random_paragraph,
+#    resolved: false
+#  )
+#end
 
 # Create Sponsored Posts
-50.times do
-  SponsoredPost.create!(
-    topic: topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
-    price: 99
-  )
-end
-sponsored_posts = SponsoredPost.all
+#50.times do
+#  SponsoredPost.create!(
+#    topic: topics.sample,
+#    title: RandomData.random_sentence,
+#    body: RandomData.random_paragraph,
+#    price: 99
+#  )
+#end
+#sponsored_posts = SponsoredPost.all
 
 # Create Advertisements
-10.times do
-  Advertisement.create!(
-    title: RandomData.random_sentence,
-    copy: RandomData.random_paragraph,
-    price: 99
-  )
-end
+#10.times do
+#  Advertisement.create!(
+#    title: RandomData.random_sentence,
+#    copy: RandomData.random_paragraph,
+#    price: 99
+#  )
+#end
 
-advertisements = Advertisement.all
+#advertisements = Advertisement.all
 
 # Create an admin user
 admin = User.create!(
@@ -83,7 +83,7 @@ admin = User.create!(
 
 # Create a member
 member = User.create!(
-  name:     'Member User',
+  name:     'Cashmere Cat',
   email:    'member@example.com',
   password: 'helloworld'
 )
